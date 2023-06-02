b5718811dc9b33026ac52a478fe561de99c4da1e
Keith Hanson <keith@keithhanson.io>
2023-06-01 19:47:06 -0500

    Now histories are stored per channel!

--- plugins/default.py
+++ plugins/default.py
@@ -29,21 +29,6 @@ connection_string = PGVector.connection_string_from_db_params(
 llm=ChatOpenAI(temperature=0)
 embeddings = OpenAIEmbeddings()
 
-vectorstore = PGVector(
-    connection_string=connection_string,
-    embedding_function=embeddings,
-    collection_name='default_chat_history_new',
-    distance_strategy=DistanceStrategy.COSINE
-)
-
-retriever = vectorstore.as_retriever()
-
-memory = VectorStoreRetrieverMemory(retriever=retriever)
-
-agent_chain = ConversationChain(
-    llm=llm,
-    memory=memory,
-    verbose=True)
 
 class Default(Plugin):
     @listen_to("(.*?)")
@@ -53,6 +38,22 @@ class Default(Plugin):
 
         if message.user_id == self.driver.user_id:
             return
+
+        vectorstore = PGVector(
+            connection_string=connection_string,
+            embedding_function=embeddings,
+            collection_name=message.channel_id,
+            distance_strategy=DistanceStrategy.COSINE
+        )
+
+        retriever = vectorstore.as_retriever()
+
+        memory = VectorStoreRetrieverMemory(retriever=retriever)
+
+        agent_chain = ConversationChain(
+            llm=llm,
+            memory=memory,
+            verbose=True)
 
         self.driver.create_post(message.channel_id, "_relaying to langchain_")
         output = agent_chain.run(message.text)
